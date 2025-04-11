80d3ca485f032e959b284213b093f7093b20979d
ApalTaalaibekova7 <apaltaalaibekova7@gmail.com>
2025-04-12 00:38:05 +0300

    in

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,7 @@
 import './style.css'
 
 
-import instagram from '../../img/icons/instagram.svg'; 
+import kwork from '../../img/icons/kwork.png'; 
 import  telegram from '../../img/icons/tg.png';
 import  linkedIn from '../../img/icons/linkedIn.svg';
 import  gitHub from '../../img/icons/gitHub.svg';
@@ -13,13 +13,13 @@ function Footer() {
             <div className="container">
                 <div className="footer__wrapper">
                     <ul className="social">
-                        <li className="social__item"><a href="#!"><img src={instagram} alt="Link"/></a></li>
                         <li className="social__item"><a href="https://github.com/ApalTaalaibekova7"><img src={gitHub} alt="Link"/></a>
                         </li>
                         <li className="social__item"><a href="https://t.me/ApalTaalaibekova"><img src={telegram} alt="Link"/></a>
                         </li>
-                        <li className="social__item"><a href="linkedin.com/in/apal-taalaibekova-815b70324"><img src={linkedIn} alt="Link"/></a>
+                        <li className="social__item"><a href="https://www.linkedin.com/in/apal-taalaibekova-815b70324/"><img src={linkedIn} alt="Link"/></a>
                         </li>
+                        <li className="social__item"><a href="https://kwork.ru/user/apaltaalaibekova7"><img src={kwork} alt="Link"/></a></li>
                         <li className="social__item"><a href="https://www.fiverr.com/s/1qL4rA5"><img src={fiverr} alt="Link"/></a>
                         </li>
                     </ul>
